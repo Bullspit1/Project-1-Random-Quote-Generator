4ef334e8e3cd2181e14ed7010e518e8706f4e769
Stephen Ditta <stephenditta@Stephens-MacBook-Pro.local>
2018-01-15 02:54:11 -0500

    fixed and finished conditional statement and display the strings of quotes on the page

--- js/script.js
+++ js/script.js
@@ -5,19 +5,19 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 var quotes = [
   {quote: "Moral indignation is jealousy with a halo.",
    source: "H. G. Wells",
-   citation: "",
-   year: "(1866-1946)"},
+   citation: "fjdfjsdkf",
+   year: 1866 + "-" + 1946},
   {quote: "Glory is fleeting, but obscurity is forever.",
    source: "Napoleon Bonaparte",
    citation: "",
-   year: "(1769-1821)"},
+   year: 1769 + "-" +1821},
   {quote: "You can avoid reality, but you cannot avoid the consequences of avoiding reality.",
-   source: "Ayn Rand (1905-1982)",
+   source: "Ayn Rand",
    citation: "",
-   year: "(1905-1982)"},
+   year: 1905 + "-" + 1982},
   {quote: "A successful man is one who can lay a firm foundation with the bricks others have thrown at him.",
    source: "David Brinkley",
-   citation: "",
+   citation: "fdfds",
    year: ""},
   {quote: "Strive not to be a success, but rather to be of value.",
    source: "Albert Einstein",
@@ -45,6 +45,32 @@ var eachYear = randomQuoteObject['year'];
 // console.log(eachSource);
 // console.log(eachCitation);
 // console.log(eachYear);
-//TODO printQuote constructs a string containing the different properties of the quote object using the following HTML template:
+
+html = '<p class="quote">' + eachQuote + '</p>';
+html += '<p class="source">' + eachSource;
+// html += '<span class="citation">' + eachCitation + '</span>';
+// html += '<span class="year">' + eachYear + '</span>';
+if (eachCitation === "" && eachYear === "") {
+  html += "";
+}
+else if(eachCitation === ""){
+  // console.log("empty citation");
+  // html -= '<span class="citation">' + eachCitation + '</span>';
+  html += '<span class="year">' + eachYear + '</span>';
+}
+else if(eachYear === ""){
+  // console.log("empty year");
+  // html -= '<span class="year">' + eachYear + '</span>';
+  html += '<span class="citation">' + eachCitation + '</span>';
+}
+else{
+  // console.log("working");
+  html += '<span class="citation">' + eachCitation + '</span>';
+  html += '<span class="year">' + eachYear + '</span>';
+}
+// html += '<span class="citation">' + eachCitation + '</span>';
+// html += '<span class="year">' + eachYear + '</span>';
+html += '</p>';
+document.getElementById('quote-box').innerHTML = html;
 // document.getElementsByClassName('quote')[0].innerHTML = eachQuote;
 }
