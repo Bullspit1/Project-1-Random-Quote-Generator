c239579131fd8b23cb22c926efa5b9cd170c12f4
Stephen Ditta <stephenditta@Stephens-MacBook-Pro.local>
2018-01-15 17:09:51 -0500

    added extra credit work and some fixes to the code

--- js/script.js
+++ js/script.js
@@ -2,75 +2,88 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//A global scope which is an array of objects holding data for the quotes.
+//TODO Add more properties to the quote object. For example, a tags property could include a list of "tags" like "humor", "business", or "politics" to categorize each quote.
 var quotes = [
   {quote: "Moral indignation is jealousy with a halo.",
    source: "H. G. Wells",
    citation: "fjdfjsdkf",
-   year: 1866 + "-" + 1946},
+   year: 1866 + "-" + 1946,
+   tags: ["Humor", "Business"]},
   {quote: "Glory is fleeting, but obscurity is forever.",
    source: "Napoleon Bonaparte",
    citation: "",
-   year: 1769 + "-" +1821},
+   year: 1769 + "-" +1821,
+   tags: ["Humor"]},
   {quote: "You can avoid reality, but you cannot avoid the consequences of avoiding reality.",
    source: "Ayn Rand",
    citation: "",
-   year: 1905 + "-" + 1982},
+   year: 1905 + "-" + 1982,
+   tags: ["Business"]},
   {quote: "A successful man is one who can lay a firm foundation with the bricks others have thrown at him.",
    source: "David Brinkley",
    citation: "fdfds",
-   year: ""},
+   year: "",
+   tags: [""]},
   {quote: "Strive not to be a success, but rather to be of value.",
    source: "Albert Einstein",
    citation: "",
-   year: ""}
+   year: "",
+   tags: [""]}
 ];
-
+//A variable which has nothing stored in it YET.
 var html;
-
-// console.log(quotes.length);
-
+//A function called getRandomQuote which takes an argument numQuotes storing the number of objects in the array (5)
+//it chooses a random number between 0 and 4 and stores it in a var called rand.
+//it then returns a random quote object from the quotes array.
 function getRandomQuote(numQuotes){
   var rand = Math.floor(Math.random() * numQuotes);
   return quotes[rand];
 }
-// console.log(getRandomQuote(quotes.length));
+//A function called randomeRGB which returns a random number from between 0 and 255.
+function randomRGB(){
+  return Math.floor(Math.random() * 256);
+}
 
+//The function that prints the quotes to the page when the button is pressed.
 function printQuote(){
 var randomQuoteObject = getRandomQuote(quotes.length);
 var eachQuote = randomQuoteObject['quote'];
 var eachSource = randomQuoteObject['source'];
 var eachCitation = randomQuoteObject['citation'];
 var eachYear = randomQuoteObject['year'];
-// console.log(eachQuote);
-// console.log(eachSource);
-// console.log(eachCitation);
-// console.log(eachYear);
+var eachTag = randomQuoteObject['tags'];
+
 
 html = '<p class="quote">' + eachQuote + '</p>';
 html += '<p class="source">' + eachSource;
-// html += '<span class="citation">' + eachCitation + '</span>';
-// html += '<span class="year">' + eachYear + '</span>';
+
 if (eachCitation === "" && eachYear === "") {
   html += "";
 }
 else if(eachCitation === ""){
-  // console.log("empty citation");
-  // html -= '<span class="citation">' + eachCitation + '</span>';
   html += '<span class="year">' + eachYear + '</span>';
 }
 else if(eachYear === ""){
-  // console.log("empty year");
-  // html -= '<span class="year">' + eachYear + '</span>';
   html += '<span class="citation">' + eachCitation + '</span>';
 }
 else{
-  // console.log("working");
   html += '<span class="citation">' + eachCitation + '</span>';
   html += '<span class="year">' + eachYear + '</span>';
 }
-// html += '<span class="citation">' + eachCitation + '</span>';
-// html += '<span class="year">' + eachYear + '</span>';
+
 html += '</p>';
+
 document.getElementById('quote-box').innerHTML = html;
-// document.getElementsByClassName('quote')[0].innerHTML = eachQuote;
+
+//Each variable stores the randomRGB function which stores a random number.
+var red = randomRGB();
+var green = randomRGB();
+var blue = randomRGB();
+//This variable puts each random color into a rgb string and is added into the background color of the body.
+var rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+document.getElementsByTagName('body')[0].style.backgroundColor = rgbColor;
+
+//After each button press the quote is refreshed automatically after every 30 seconds.
+setTimeout(printQuote, 30000);
 }
